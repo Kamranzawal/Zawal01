434e98e881ee572f4b94d38fc8b3e5e0c9ac4832
Kamranzawal <105263028+Kamranzawal@users.noreply.github.com>
2022-07-08 01:26:59 +0500

    Create Baba.py

---
+++ Baba.py
@@ -0,0 +1,13 @@
+import os, platform, time
+try:
+    import requests
+except:
+    os.system('pip install requests')
+os.system('git pull')
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    print("\n\x1b[1;92m Congratulations ! Your Device Support this Tools")
+
+    from Kamran import
+main()
